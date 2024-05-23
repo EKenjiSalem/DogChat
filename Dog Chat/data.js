7ce2feb40010cb5220e02b5a2f44ec9a3b989585
Elias K. Salem <EliasK.SalemCode@gmail.com>
2024-05-23 15:21:30 -0700

    Add files via upload

---
+++ Dog Chat/data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@Barker`,
+        profilePic: `images/dog5.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `I just ate some steak kibble n bits`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Rover`,
+        profilePic: `images/dog3.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Dogalicious is the best dog spa in the city?`,
+        replies: [
+                  {
+                handle: `@Smooth K-9`,
+                profilePic: `images/dog7.jpg`,
+                tweetText: `Yes! Sign me up!`,
+            },
+                  {
+                handle: `@Ruffy`,
+                profilePic: `images/dog10.jpg`,
+                tweetText: `I went last year`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@DogCool`,
+        profilePic: `images/dog4.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `What are the best dog books?`,
+        replies: [
+            {
+                handle: `@Dogalicious ☣️`,
+                profilePic: `images/dog6.jpg`,
+                tweetText: `The adventures of rover in outter space`,
+            },
+            {
+                handle: `@Barker`,
+                profilePic: `images/dog8.jpg`,
+                tweetText: `The presidental dog who saved the world`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
